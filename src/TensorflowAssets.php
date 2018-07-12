1d6b4d6d4fc59ca09416e90b441bf148d0348581
Luis Gonzalez <luisarmando1234@gmail.com>
2018-07-12 11:42:49 -0500

    Update TensorflowAssets.php

--- src/TensorflowAssets.php
+++ src/TensorflowAssets.php
@@ -5,8 +5,9 @@
  * Date: 6/10/17
  * Time: 03:05 PM
  */
-namespace app\components;
+namespace inquid\tensorflowjs;
 use yii\web\AssetBundle;
+
 class TensorflowAssets extends AssetBundle
 {
     public $js = ['https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@0.11.6'];
